0f23af93326c2c772ffef7f9fa10f0b7257ba891
Kishan Singh <kishan995694@gmail.com>
2024-10-13 23:40:33 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,35 +7,35 @@ import java.net.Socket;
 
 public class Main {
 
-  public static boolean readPathOfHTTPRequest(BufferedReader bufferedReader)
+  public static void readPathOfHTTPRequest(BufferedReader bufferedReader, PrintWriter printWriter)
       throws IOException {
     String requestLine = bufferedReader.readLine();
     if (requestLine != null && !requestLine.isEmpty()) {
       System.out.println("RequestedLine ==> " + requestLine);
 
       String[] requestParts = requestLine.split(" ");
-      if (!requestParts[1].equals("/")) {
-        return false;
+      String response;
+      if (requestParts[1].contains("echo")) {
+        String message = requestParts[1].split("/")[1];
+        response = String.format("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
+            message.length(), message);
+      } else if (!requestParts[1].contains("/")) {
+        response = "HTTP/1.1 404 Not Found\r\n\r\n";
+      } else {
+        response = "HTTP/1.1 200 OK\r\n\r\n";
       }
+      printWriter.write(response);
+      printWriter.flush();
+      printWriter.close();
     }
 
-    return true;
   }
 
   public static void sendResponseToServer(Socket socket) throws IOException {
     PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    readPathOfHTTPRequest(bufferedReader, printWriter);
 
-    StringBuilder msg;
-    if (readPathOfHTTPRequest(bufferedReader)) {
-      msg = new StringBuilder("HTTP/1.1 200 OK\r\n\r\n");
-    } else {
-      msg = new StringBuilder("HTTP/1.1 404 Not Found\r\n\r\n");
-    }
-
-    printWriter.write(msg.toString());
-    printWriter.flush();
-    printWriter.close();
   }
 
   public static void main(String[] args) {
